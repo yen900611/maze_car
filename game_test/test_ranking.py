1aada3d836ecb8667348128f6ef719329b88d808
Yen900611 <yen900611@gmail.com>
2021-04-13 10:15:18 +0800

    temp

---
+++ game_test/test_ranking.py
@@ -0,0 +1,17 @@
+import unittest
+
+from game_core.car import Car
+from game_core.mazeMode import MazeMode
+
+
+class MyTestCase(unittest.TewstCase):
+    def setUp(self) -> None:
+        pass
+
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
