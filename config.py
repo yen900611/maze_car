70174ff5f724e5d83049a4956756ae8536500086
Yen900611 <yen900611@gmail.com>
2021-05-05 15:59:57 +0800

    revise config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ GAME_PARAMS = {
                  "when either side reaches it.[default: %(default)s]")
     },
     "game_type": {
-        "choices": ("MAZE", "MOVE_MAZE"),
+        "choices": ("MAZE", "MOVE_MAZE", "PRACTICE"),
         "metavar": "game_mode",
         "nargs": "?",
         "default": "MAZE",
